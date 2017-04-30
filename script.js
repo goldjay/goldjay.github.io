43ad47d550f5e5838f3d67b69156d76bd617e0f0
goldjay <jay.steingold@gmail.com>
2017-04-29 18:48:11 -0700

    Added images and links to projects

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 
-
-$(function() {
-  $('progress').each(function() {
-    var max = $(this).val();
-    console.log("WOAH!");
-    //$(this).val(0).animate({ value: max }, { duration: 2000, easing: 'easeOutCirc' });
-			});
-});
-
 $(document).ready(function(){
-    $(".fa-arrow-down").click(function(){
-      console.log("YO");
+    $(".fa-envelope").click(function(){
+      console.log("YO")
         $("form").toggle();
     });
 });
